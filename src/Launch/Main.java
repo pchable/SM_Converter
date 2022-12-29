afb7c9be7787b504140e44f0ff4269f78effe08d
patrick CHABLE <patrick.chable@free.fr>
2022-12-29 15:01:28 +0100

    Reading Config file

--- src/Launch/Main.java
+++ src/Launch/Main.java
@@ -8,26 +8,9 @@ public class Main
 
         public static void main(String[] args)
         {
-            String sourceFile = "Not Found";
-
             System.out.println("Bienvenue dans le convertisseur" );
-
-            for( int index = 0; index < args.length; index++  )
-            {
-                String parameter = args[index];
-
-                switch( parameter ) {
-                    case "-Source":
-                        sourceFile = args[index + 1];
-                        break;
-                }
-            }
-
-            System.out.println("Source: " + sourceFile );
-            SupplierReader _Reader = new SupplierReader( sourceFile );
-            _Reader.read( sourceFile );
-
+            SupplierReader reader = new SupplierReader();
+            reader.read();
 
         }
-
 }
--- src/Main/SupplierReader.java
+++ src/Main/SupplierReader.java
@@ -1,41 +1,82 @@
 package Main;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.util.Properties;
 
 public class SupplierReader
 {
-    String _FileName;
-    String _Line = "";
-    String _SplitBy = ",";
-    int _MaxRead = 10;
+    Properties _Properties;
 
-    public SupplierReader( String pFileName )
+    public SupplierReader( )
     {
-        _FileName = pFileName;
+        try
+        {
+            _Properties = new Properties();
+            FileInputStream ip = new FileInputStream("data/SupplierReader.config");
+            _Properties.load(ip);
+        }
+        catch( Exception pException )
+        {
+            System.out.println("Failed reading supplier config file" );
+        }
     }
 
-    public void read( String pFileName )
+    public void read()
     {
         int count = 0;
+        String line;
+        boolean doRead = true;
+
+        //
+        // ---- Get Properties
+        //
+        int maxRead = Integer.parseInt( _Properties.getProperty("MaxRead"));
+        String sourceFile = _Properties.getProperty("Source");
+
         try
         {
 
-            BufferedReader br = new BufferedReader(new FileReader( _FileName ));
-            while( count < _MaxRead )
+            BufferedReader br = new BufferedReader(new FileReader( sourceFile ) );
+            while( doRead )
             {
-                _Line = br.readLine();
-                System.out.println("Line: " + _Line);
-                count++;
+                line = br.readLine();
+
+                if( line != null )
+                {
+                    processLine( line );
+                    count++;
+                }
+
+                if( ( line == null)  || ( count > maxRead ) )
+                {
+                    doRead = false;
+                }
             }
         }
         catch( Exception pException )
         {
-            System.out.println("Error reading file: " + _FileName );
+            System.out.println("Error reading file: " + sourceFile );
             System.out.println("Error " + pException.getMessage() );
         }
     }
 
+    protected void processLine(@NotNull String pLine )
+    {
+        //
+        // ---- Get Properties
+        //
+        String columnSplitter = _Properties.getProperty( "ColumnSplitter");
 
+        String columns[] = pLine.split( columnSplitter );
+        for( String header : columns )
+        {
+            System.out.println("header: " + header );
+        }
+
+    }
 
 }
